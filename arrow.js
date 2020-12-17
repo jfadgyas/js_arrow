1d5d6333e199842a0355ef1e2663ba486c151cd4
jfadgyas <djdeville72@yahoo.com>
2020-12-17 12:04:49 +0100

    js_arrow_function

---
+++ arrow.js
@@ -0,0 +1,19 @@
+// zet de functie om naar een arrow functie
+const ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!");
+ 
+ ikRockArrowFunctions();
+
+ //schrijf de onderstaande functie om naar een arrow functie op 1 regel
+ const fivePlusSeven = () => 5 + 7;
+ 
+ fivePlusSeven();
+
+//de params a en b bij-elkaar optelt.
+const myFunction = (a,b) => a+b;
+
+//addfive
+const addFive = a => a+5;
+
+//createobject
+const createObject = () => ({greeting: 'hoi'});
+console.log(createObject())
